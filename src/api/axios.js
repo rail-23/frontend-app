3e7d7269950cf046576314fc823f1e7201b184a0
rail-23 <railgonzalesaldana603@gmail.com>
2024-11-29 04:01:41 -0400

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,18 +1,22 @@
 import axios from 'axios';
 
+// Configuración de axios
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', // URL del backend
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000', // URL dinámica para backend
 });
 
 // Interceptor para incluir el token en cada solicitud
-instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers['x-access-token'] = token;
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['x-access-token'] = token; // Header personalizado
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default instance;
