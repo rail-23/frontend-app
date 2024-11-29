779b9de92ef59d5ae1b39cca4c7219dbca307924
rail-23 <railgonzalesaldana603@gmail.com>
2024-11-29 04:24:46 -0400

    Update decodeJWT.js

--- src/utils/decodeJWT.js
+++ src/utils/decodeJWT.js
@@ -1,17 +1,20 @@
-export function decodeJWT(token) {
-    try {
-        const base64Url = token.split('.')[1];
-        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const jsonPayload = decodeURIComponent(
-            atob(base64)
-                .split('')
-                .map((c) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
-                .join('')
-        );
+import jwtDecode from 'jwt-decode';
 
-        return JSON.parse(jsonPayload);
-    } catch (error) {
-        console.error("Error al decodificar el token JWT:", error);
-        return null;
+const decodeToken = () => {
+  try {
+    const token = localStorage.getItem('token');
+    if (!token) throw new Error('Token no encontrado');
+
+    const decoded = jwtDecode(token);
+
+    if (!decoded.roles) {
+        throw new Error('El token no contiene roles válidos');
     }
-}
+
+    console.log('Token decodificado:', decoded);
+    return decoded;
+  } catch (error) {
+    console.error('Error al decodificar el token JWT:', error.message);
+    return null;
+  }
+};
