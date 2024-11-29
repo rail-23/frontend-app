5b9802b3ac5c52f7059209a504218b9df58b6aa2
rail-23 <railgonzalesaldana603@gmail.com>
2024-11-29 08:26:38 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true, // Transforma dependencias con mezcla de ESM y CommonJS
+    plugins: [react()],
+    resolve: {
+        alias: {
+            // Asegúrate de usar un alias correcto para jwt-decode
+            'jwt-decode': 'jwt-decode/build/esm/index.js',
+        },
     },
-  },
 });
