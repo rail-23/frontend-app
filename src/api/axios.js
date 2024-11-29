10765e8eadb771ee647e51e36c7ff6163578e72b
rail-23 <railgonzalesaldana603@gmail.com>
2024-11-29 09:03:57 -0400

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // Usar la variable de entorno
+  baseURL: https://react-mern-express.onrender.com// Usar la variable de entorno
 });
 
 instance.interceptors.request.use(
