8057d6ce648518a1178841cf0a7c563cd603b8e8
rail-23 <railgonzalesaldana603@gmail.com>
2024-11-29 04:58:12 -0400

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../api/services';
-import { decodeJWT } from '../utils/decodeJWT';
+import  decodeJWT  from '../utils/decodeJWT';
 import '../css/Login.css';
 
 const Login = () => {
