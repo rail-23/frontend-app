9b1f216ceba9f6645ea200c9b8b9df9635edf94f
rail-23 <railgonzalesaldana603@gmail.com>
2024-11-29 23:58:11 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     optimizeDeps: {
-        include: ['jwt-decode'],
+        include: ['jwt-decode', 'react-router-dom'], // Asegúrate de incluir react-router-dom
+    },
+    build: {
+        rollupOptions: {
+            external: ['react-router-dom'], // Si es necesario, declara explícitamente los paquetes externos
+        },
     },
 });
