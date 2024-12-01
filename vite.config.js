cd7103079289b8fea3addff1b13925e0cf51d510
rail-23 <railgonzalesaldana603@gmail.com>
2024-12-01 00:51:20 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    optimizeDeps: {
-        include: ['jwt-decode', 'react-router-dom'], // Asegúrate de incluir react-router-dom
-    },
-    build: {
-        rollupOptions: {
-            external: ['react-router-dom'], // Si es necesario, declara explícitamente los paquetes externos
-        },
-    },
-});
+  plugins: [react()],
+  build: {
+    outDir: 'dist', 
+    sourcemap: true, 
+  }
+})
